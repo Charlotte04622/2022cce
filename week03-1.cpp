40c46b0ce607fdad3ab2048ab0a0c3d1aee3200f
Charlotte <112918717+Charlotte04622@users.noreply.github.com>
2022-09-20 11:05:55 +0800

    week03

--- week03-1.cpp
+++ week03-1.cpp
@@ -9,5 +9,5 @@ int main()
     printf(" %d - %d 是 %d\n", a, b, a-b);
     printf(" %d * %d 是 %d\n", a, b, a*b);
     printf(" %d / %d 是 %d\n", a, b, a/b);
-    printf(" %d 除法 %d 的除數是 %d\n", a, b, a%b);
+    printf(" %d 除法 %d 的餘數是 %d\n", a, b, a%b);
 }
